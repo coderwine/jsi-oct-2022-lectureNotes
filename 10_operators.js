874e8fd97a1cf93928fbe0730702b98ee363683b
Eric Winebrenner <ejwinebrenner@gmail.com>
2022-10-20 18:21:14 -0400

    updated operator notes

--- 10_operators.js
+++ 10_operators.js
@@ -27,3 +27,42 @@ console.log('3' != 3);
 // Strict Not Equal To
 console.log('3' !== 3);
 
+// Greater Than
+3 > 2;
+
+// Less Than
+2 < 3;
+
+// Great Than or Equal To
+3 >= 2;
+
+// Less Than or Equal To
+2 <= 3;
+
+// And
+2 && 2;
+
+// Or
+2 || 3;
+
+//! Logical Operators
+// Addition
+r = 2;
+r += 1; // r = r + 1;
+
+// Subtraction
+r -= 1; // r = r - 1;
+
+// Multiplication
+r *= 2; // r = r * 2;
+
+// Division
+r /= 4; // r = r / 4;
+
+// Remainder (Modulus)
+r %= 1; //  r = r % 1;
+
+console.log(7 % 3);
+
+// Exponential 
+r **= 1; // r = r ** 1;
